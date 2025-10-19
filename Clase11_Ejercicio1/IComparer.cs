b7f89290b43d09474d2f67c07e7037c3d16e892a
Ivan Lopez <ialmendra@infobae.com>
2025-10-19 10:58:08 -0300

    Agregar archivos de proyecto.

---
+++ Clase11_Ejercicio1/IComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase11_Ejercicio1
+{
+    internal class IComparer
+    {
+        public class Employee : IComparable<Employee>
+        {
+            public string Name { get; set; }
+            public double Salary { get; set; }
+
+            public int CompareTo(Employee other)
+            {
+                // Ordenar Ascendentemente
+                // return this.Salary.CompareTo(other.Salary);
+
+                // Ordenar Desendentemente
+                // return other.Salary.CompareTo(this.Salary);
+
+                // Ordenar por nombre
+                // return this.Name.CompareTo(other.Name);
+
+                /*
+                 * Explicado en el libro
+                    a. Si el valor es menor que cero significa que X es menor que Y.
+                    b. Si el valor es cero significa que X en igual a Y.
+                    c. Si el valor es mayor que cero significa que X es mayor que Y.
+                 */
+
+                // Si el salario es igual, entonces ordena por nombre
+                // -1 -> menor
+                // 0 -> igual
+                // 1 -> mayor
+                int salaryComparation = other.Salary.CompareTo(this.Salary);
+                if (salaryComparation == 0) return this.Name.CompareTo(other.Name);
+                return other.Salary.CompareTo(this.Salary);
+            }
+        }
+    }
+}
---
+++ Clase11_Ejercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static Clase11_Ejercicio1.IComparer;
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee { Name = "Alice", Salary = 60000 },
+    new Employee { Name = "Carla", Salary = 50000 },
+    new Employee { Name = "Bob", Salary = 50000 },
+    new Employee { Name = "Analia", Salary = 85000 },
+};
+
+employees.Sort();
+
+foreach (var emp in employees)
+{
+    Console.WriteLine($"{emp.Name} - {emp.Salary}");
+}
